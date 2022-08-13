caafbb03650f7966da4e0a833a25e290f2b4bb48
victorFresneda <vafresnedah@hotmail.com>
2022-08-12 23:46:38 -0500

    Ñ¿+

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,13 +4,25 @@ import { authReducer } from "./authReducer"
 
 import { types } from "../types/types";
 
-const initialState = {
-  logged: false,
+
+const init = () => {
+  const user = JSON.parse( localStorage.getItem('user'));
+
+  return {
+    logged: !!user,
+    user: user,
+  }
 }
 
 export const AuthProvider = ({ children }) => {
 
+  const [ authState, dispatch ] = useReducer( authReducer, {}, init);
+
+
   const login = ( name = '') => {
+
+    const user = { id: 'ABC', name}
+    
      const action = {
       type: types.login,
       payload: {
@@ -19,12 +31,13 @@ export const AuthProvider = ({ children }) => {
       }
      }
 
+     localStorage.setItem('user', JSON.stringify( user ));
+
      dispatch(action);
   }
 
   
 
-   const [ authState, dispatch ] = useReducer( authReducer, initialState);
 
    return (
     
