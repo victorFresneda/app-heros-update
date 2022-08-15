e2c5698d22c3a39d13e14760ba8bc02dc3756cf5
victorFresneda <vafresnedah@hotmail.com>
2022-08-14 20:20:50 -0500

    [add] function for logout

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -19,12 +19,7 @@ export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
-            <Link 
-                className="navbar-brand" 
-                to="/"
-            >
-                Asociaciones
-            </Link>
+         
 
             <div className="navbar-collapse">
                 <div className="navbar-nav">
